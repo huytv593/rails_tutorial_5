b47f703efffe76277343835ccfee433fe74f8708
Huy Tran <huytv593@gmail.com>
2016-08-08 17:53:26 +0700

    Chapter 6. Modeling Users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create([{name: 'First User', email: 'user1@foor.bar', password: '123456', password_confirmation: '123456'}, {name: 'Second User', email: 'user2@foor.bar', password: '123456', password_confirmation: '123456'}])
+User.create([{name: 'Third User', email: 'user3@foor.bar', password: '123456', password_confirmation: '123456'}, {name: 'Fourth User', email: 'user4@foor.bar', password: '123456', password_confirmation: '123456'}])
