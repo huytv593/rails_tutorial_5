808e5cdf92b9c7240d80c473d0b5e2dbb996b6b4
Huy Tran <huytv593@gmail.com>
2016-08-09 14:08:17 +0700

    simple login

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   get 'about', to: 'static_pages#about'
   get 'contact', to: 'static_pages#contact'
   get 'sign_up', to: 'users#new'
-
+  get 'sign_in', to: 'sessions#new'
+  post 'sign_in', to: 'sessions#create'
+  delete 'sign_out', to: 'sessions#destroy'
   resources :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
