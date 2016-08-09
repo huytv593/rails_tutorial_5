a792c13ed4e5b74a0024cd515bd6dcc3b187ad3f
Huy Tran <huytv593@gmail.com>
2016-08-09 08:53:31 +0700

    Add secure for user password

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,4 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create([{name: 'First User', email: 'user1@foor.bar', password: '123456', password_confirmation: '123456'}, {name: 'Second User', email: 'user2@foor.bar', password: '123456', password_confirmation: '123456'}])
-User.create([{name: 'Third User', email: 'user3@foor.bar', password: '123456', password_confirmation: '123456'}, {name: 'Fourth User', email: 'user4@foor.bar', password: '123456', password_confirmation: '123456'}])
+User.create(name: "Michael Hartl", email: "mhartl@example.com", password: "foobar", password_confirmation: "foobar")
